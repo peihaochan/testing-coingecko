3ff440bafa23090895806c7e0b352a79e390092c
github-personal <jchan1993@hotmail.com>
2024-05-28 01:55:26 +0800

    fix: recall API if fail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,63 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-// check why calling twice each
-// fetch once again(the ones unable to fecth) if some errors
-// must fetch all 3, retry and make sure all are fetch successful - stop calling after X amount
-// check strict mode
-
+interface iPrice {
+  ethereum: null | number;
+  optimism: null | number;
+  arbitrum: null | number;
+}
 export default function Home() {
-  const [data, setData] = useState<any>({
+  const [price, setPrice] = useState<iPrice>({
     ethereum: null,
     optimism: null,
     arbitrum: null,
   });
 
-  const assetArr = ["ethereum", "optimism", "arbitrum"];
+  const assetArr = ["ethereum", "optimism", "arbitrum"] as const;
+  type AssetType = (typeof assetArr)[number];
+
+  const hasFetchedData = useRef<Record<AssetType, boolean>>({
+    ethereum: false,
+    optimism: false,
+    arbitrum: false,
+  });
+
+  const maxApiCalls = 3; // Setting the maximum number of API calls
+  const apiCallTimeout = 1000; // Timeout for API calls in milliseconds
+  let apiCallCount = 0;
 
   useEffect(() => {
-    const getAssets = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.coingecko.com/api/v3/simple/price?ids=${"ethereum"}&vs_currencies=usd`
-        );
-        console.log("response", response);
-        // for (let i = 0; i < assetArr.length; i++) {
-        //   if (data[assetArr[i]] === null) {
-        //     const response = await axios.get(
-        //       `https://api.coingecko.com/api/v3/simple/price?ids=${assetArr[i]}&vs_currencies=usd`
-        //     );
+    const fetchData = async () => {
+      for (let i = 0; i < assetArr.length; i++) {
+        if (
+          price[assetArr[i]] === null &&
+          !hasFetchedData.current[assetArr[i]] &&
+          apiCallCount < maxApiCalls
+        ) {
+          try {
+            const response = await axios.get(
+              `https://api.coingecko.com/api/v3/simple/price?ids=${assetArr[i]}&vs_currencies=usd`
+            );
 
-        //     setTimeout(response as any, 5000);
-        //     if (response.data.code === 200) {
-        //       setData({ ...data, [data[i]]: response.data[assetArr[i]] });
-        //     } else {
-        //       console.error("no response");
-        //     }
-        //   }
-        // }
-      } catch (error) {
-        console.log(error);
+            if (response.status === 200) {
+              setPrice((prevPrice: iPrice) => ({
+                ...prevPrice,
+                [assetArr[i]]: response.data[assetArr[i]].usd,
+              }));
+              hasFetchedData.current[assetArr[i]] = true;
+              apiCallCount++;
+              setTimeout(fetchData, apiCallTimeout);
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }
       }
     };
-    getAssets();
+
+    fetchData();
   }, []);
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        {assetArr.map((asset) => (
-          <div key={asset}>
-            {asset}: {data[asset] !== null ? data[asset] : "Loading..."}
-          </div>
-        ))}
-      </div>
-    </main>
-  );
+  return <div></div>;
 }
